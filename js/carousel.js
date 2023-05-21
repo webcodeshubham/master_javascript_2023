a8023d91d5557d46dcb13cce780f0052be80ea29
shubhamshrivastavathefullstack@gmail.com <shubhamforester23@outlook.com>
2023-05-21 19:57:34 +0530

    carousel App Completed

--- js/carousel.js
+++ js/carousel.js
@@ -17,14 +17,16 @@ slides.forEach((slide, index) => {
 const imageSlider = () => {
   slides.forEach((slide, index) => {
     // We want to move or translate the slide on x axis, with magnitude and direction as (- or +).
+    // Translating is only moving not changing the static position of the slide.
     slide.style.transform = `translateX(-${counter * 100}%)`;
   });
 };
 
-// Two Secenarios, when someone clicks on NEXT Button =>
-// #1. When the Counter is 0 => Non-Functional.
-// #2. When the Counter is greater than 0 => Functional.
-// You are decreasing the counter and translating forwards with 100% width, it will take the reference from parent left container.
+// Secenarios, when someone clicks on PREV Button =>
+// #1. Counter will increase or remains 0 (initial value). It will never be negative.
+// #2. When the Counter becomes equals to 0 => Non-Functional/Counter will not decrease.
+// #3. When the Counter is greater than 0 => Functional.
+// You are decreasing the counter and translating forwards with 100% width, it will take the reference from parent left container(ORIGIN 0 (it will shift x% left of origin)).
 // The counter value will no more decrease, once it is reached to index = 0 (Image).
 const prev = () => {
   if (counter > 0) {
@@ -33,15 +35,21 @@ const prev = () => {
   }
 };
 
-// Three Secenarios, when someone clicks on NEXT Button =>
-// #1. When the Counter is 0 => Functional.
-// #2. When the Counter is 4 => Functional.
-// #3. When the Counter becomes equals to (slides.length - 1) => Non-Functional.
-// You are increasing the counter and translating backwards with 100% width, it will take the reference from parent left container.
-// The counter value will no more increase, once all slides have finished.
+// Secenarios, when someone clicks on NEXT Button =>
+// #1. Counter will increase or remains 0 (initial value).
+// #2. When the Counter becomes equals to (slides.length - 1) => Non-Functional/Counter will not increase.
+// #3. When the Counter is less than (slides.length - 1) => Functional/Counter will Increase.
+// #4. When the Counter is 4 => Functional.
+// You are increasing the counter and translating backwards with 100% width, it will take the reference from parent left container(ORIGIN 0 (it will shift x% left of origin)).
+// The counter value will no more increase, once all slides have finished or we have reached to final index value.
 const next = () => {
   if (counter < slides.length - 1) {
     counter += 1;
     imageSlider();
   }
 };
+
+
+
+
+
